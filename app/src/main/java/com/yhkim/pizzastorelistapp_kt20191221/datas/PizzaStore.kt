5641ec75b9657312f7b5c0a30d79bee16f836f15
kvoyant <kvoyant@gmail.com>
2019-12-21 15:35:13 +0900

    피자가게의 근거 데이터 클래스 설정

---
+++ app/src/main/java/com/yhkim/pizzastorelistapp_kt20191221/datas/PizzaStore.kt
@@ -0,0 +1,9 @@
+package com.yhkim.pizzastorelistapp_kt20191221.datas
+
+class PizzaStore(storeName:String, phoneNum:String, logoUrl:String) {
+
+    var storeName = storeName
+    var phoneNum = phoneNum
+    var logoUrl = logoUrl
+
+}
