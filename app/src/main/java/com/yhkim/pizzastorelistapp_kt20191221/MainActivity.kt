8ab7082a3d1579e40322a39f0fd741ffcfcdd2e7
kvoyant <kvoyant@gmail.com>
2019-12-21 15:58:04 +0900

    리스트뷰의 한줄 아이템 까지 작성

--- app/src/main/java/com/yhkim/pizzastorelistapp_kt20191221/MainActivity.kt
+++ app/src/main/java/com/yhkim/pizzastorelistapp_kt20191221/MainActivity.kt
@@ -2,8 +2,15 @@ package com.yhkim.pizzastorelistapp_kt20191221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yhkim.pizzastorelistapp_kt20191221.adapters.PizzaStoreAdapter
+import com.yhkim.pizzastorelistapp_kt20191221.datas.PizzaStore
 
 class MainActivity : BaseActivity() {
+
+    var pizzaStoreList = ArrayList<PizzaStore>()
+
+    var pizzaStoreAdapter:PizzaStoreAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +22,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        addPizzaStores()
+
+        pizzaStoreAdapter = PizzaStoreAdapter(this, R.layout.pizza_list_item, pizzaStoreList)
     }
 
+    fun addPizzaStores() {
+        pizzaStoreList.add(PizzaStore("피자헛", "02-4114-4342", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
+    }
 }
---
+++ app/src/main/java/com/yhkim/pizzastorelistapp_kt20191221/adapters/PizzaStoreAdapter.kt
@@ -0,0 +1,35 @@
+package com.yhkim.pizzastorelistapp_kt20191221.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.yhkim.pizzastorelistapp_kt20191221.R
+import com.yhkim.pizzastorelistapp_kt20191221.datas.PizzaStore
+
+class PizzaStoreAdapter(context:Context, resId:Int, list:ArrayList<PizzaStore>) : ArrayAdapter<PizzaStore>(context, resId, list) {
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if(tempRow == null) {
+            tempRow = inf.inflate(R.layout.pizza_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        var data = mList.get(position)
+
+        var pizzaNm = row.findViewById<TextView>(R.id.pizzaCompNm)
+
+        pizzaNm.text = data.storeName
+
+        return row
+
+    }
+}
